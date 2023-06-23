9c58ec494dbcf4dc661bc2a896c3809c2413563e
nestor benavidez <nestor.benavidez@gmail.com>
2023-06-23 01:57:36 +0100

    Fix answer, rearrange project, add specs

--- spec/text_formatter_spec.rb
+++ spec/text_formatter_spec.rb
@@ -1,15 +1,17 @@
-require_relative 'text_formatter'
+# spec/text_formatter_spec.rb
+require 'spec_helper'
+require 'text_formatter'
 
 RSpec.describe TextFormatter do
   describe '.format_text' do
     it 'breaks the text into lines with a maximum character limit' do
       text = "In 1991, while studying computer science at University of Helsinki, Linus Torvalds began a project that later became the Linux kernel. He wrote the program specifically for the hardware he was using and independent of an operating system because he wanted to use the functions of his new PC with an 80386 processor. Development was done on MINIX using the GNU C Compiler."
 
-      expected_result = "In 1991, while studying computer\nscience at University of Helsinki,\nLinus Torvalds began a project that\nlater became the Linux kernel. He wrote\nthe program specifically for the\nhardware he was using and independent\nof an operating system because he\nwanted to use the functions of his new\nPC with an 80386 processor.\nDevelopment was done on MINIX using\nthe GNU C Compiler."
+      expected_result = "In 1991, while studying computer science\nat University of Helsinki, Linus\nTorvalds began a project that later\nbecame the Linux kernel. He wrote the\nprogram specifically for the hardware he\nwas using and independent of an\noperating system because he wanted to\nuse the functions of his new PC with an\n80386 processor. Development was done on\nMINIX using the GNU C Compiler."
 
       result = TextFormatter.format_text(text, 40)
 
-      expect(result).to eq(expected_result)
+      expect(result.strip).to eq(expected_result.strip)
     end
   end
 end
